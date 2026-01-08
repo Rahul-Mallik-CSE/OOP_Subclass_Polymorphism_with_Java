0c126c8a2ecc5afecbe9f2462fe5bc6a577c13d2
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-08 10:17:32 +0600

    Add Bird class to demonstrate subclass polymorphism

--- src/App.java
+++ src/App.java
@@ -8,6 +8,13 @@ class Animal {
     }
 }
 
+class Bird extends Animal {
+    String type;
+    void eat() {
+        System.out.println("Bird is eating seeds");
+    }
+}
+
 
 public class App {
     public static void main(String[] args) throws Exception {
