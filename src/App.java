1a3e841e795440e2e9b705b1f6350832794d78ba
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-08 10:17:01 +0600

    Add Animal class to demonstrate subclass polymorphism

--- src/App.java
+++ src/App.java
@@ -1,3 +1,14 @@
+
+//subclass polymorphism example
+
+class Animal {
+    String color;
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
